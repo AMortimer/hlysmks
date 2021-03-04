c9a91a7795b0430206b0f8d8a37c4dbdc7a63a67
Andrew Mortimer <a-mortimer@live.co.uk>
2021-03-04 18:12:31 +0000

    Configured buttons on home.html to include Mailing List button Positioned buttons differently (away from my face) Attempted to blur background when pop is open, more complex than I'd hoped. Next steps: add Media Queries to make site usable on phone

--- hlysmks.js
+++
@@ -1,7 +0,0 @@
-function openForm() {
-    document.getElementById("contact-form").style.display = "block";
-  }
-  
-function closeForm() {
-    document.getElementById("contact-form").style.display = "none";
-  }
---
+++ javascript/hlysmks.js
@@ -0,0 +1,14 @@
+function openContactForm() {
+    document.getElementById("contact-form").style.display = "block";
+    }
+  function closeContactForm() {
+    document.getElementById("contact-form").style.display = "none";
+  }
+
+function openMailingForm() {
+    document.getElementById("mailing-list-form").style.display = "block";
+  }
+  function closeMailingForm() {
+    document.getElementById("mailing-list-form").style.display = "none";
+  }
+  
